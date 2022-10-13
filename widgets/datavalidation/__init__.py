f6c6afee699d259d2f4c2d7116553d9e58ecd4b1
JacksonBernier523 <happysmile911003@gmail.com>
2022-10-13 23:54:31 +1000

    completed

---
+++ widgets/datavalidation/__init__.py
@@ -0,0 +1,17 @@
+NAME = "Data validation"
+
+ID = "Orange.widgets.datavalidation"
+
+DESCRIPTION = """Widgets for data manipulation."""
+
+LONG_DESRIPTION = """
+This category contains widgets for data manipulation. This includes
+loading, importing, saving, preprocessing, selection, etc.
+
+"""
+
+ICON = "icons/Preprocess.svg"
+
+BACKGROUND = "#FFD39F"
+
+PRIORITY = 7
